782eb2c3c37e4f482a7bfbb72fe65349773eb3b0
mbenoukaiss <marwane.benou-kaiss@etu.u-bordeaux.fr>
2018-11-02 16:13:36 +0100

    Loading tiles from image

--- cpp/tilemap/main.cpp
+++ cpp/tilemap/main.cpp
@@ -1,27 +1,42 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv) {
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML");
+    window.setFramerateLimit(60);
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML", sf::Style::Default, settings);
+    sf::Texture tiles;
+    if(!tiles.loadFromFile("assets/rpgtiles.png"))
+        throw std::runtime_error("Failed to load RPG tiles");
 
-    sf::CircleShape shape(50.0f, 100);
-    shape.setPosition(50, 50);
-    shape.setFillColor(sf::Color(40, 120, 225));
-    shape.setOutlineThickness(10.0f);
-    shape.setOutlineColor(sf::Color(21, 96, 193));
+    sf::VertexArray vertices(sf::Quads, 8);
+    vertices[0].position = sf::Vector2f(0.0f, 0.0f);
+    vertices[1].position = sf::Vector2f(200.0f, 0.0f);
+    vertices[2].position = sf::Vector2f(200.0f, 200.0f);
+    vertices[3].position = sf::Vector2f(0.0f, 200.0f);
+    vertices[0].texCoords = sf::Vector2f(160.0f, 288.0f);
+    vertices[1].texCoords = sf::Vector2f(192.0f, 288.0f);
+    vertices[2].texCoords = sf::Vector2f(192.0f, 320.0f);
+    vertices[3].texCoords = sf::Vector2f(160.0f, 320.0f);
+
+    vertices[4].position = sf::Vector2f(0.0f, 0.0f);
+    vertices[5].position = sf::Vector2f(200.0f, 0.0f);
+    vertices[6].position = sf::Vector2f(200.0f, 200.0f);
+    vertices[7].position = sf::Vector2f(0.0f, 200.0f);
+    vertices[4].texCoords = sf::Vector2f(480.0f, 128.0f);
+    vertices[5].texCoords = sf::Vector2f(512.0f, 128.0f);
+    vertices[6].texCoords = sf::Vector2f(512.0f, 160.0f);
+    vertices[7].texCoords = sf::Vector2f(480.0f, 160.0f);
 
     while(window.isOpen()) {
         sf::Event event = {};
 
         while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed || event.type == sf::Event::LostFocus)
+            if(event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear(sf::Color(51, 51, 51));
-        window.draw(shape);
+        window.draw(vertices, &tiles);
         window.display();
     }
 
